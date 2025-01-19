c0a1564a3ca61c10eee5b7a759a5d2216af7e0d0
ayush <avora21@student.ubc.ca>
2025-01-18 23:49:47 -0800

    almost MVP

--- arduino/py_receive.py
+++ arduino/py_receive.py
@@ -1,13 +1,32 @@
 import serial
 import time
 import requests
+import numpy as np
+from datetime import datetime, timedelta
+
+from scipy.ndimage import gaussian_filter1d
 
 # Replace 'COMX' with your Arduino's port (e.g., COM3 on Windows, /dev/ttyUSB0 or /dev/ttyACM0 on Linux/Mac)
-arduino_port = '/dev/tty.usbmodem1101'  
+# arduino_port = '/dev/tty.usbmodem1101'  
+arduino_port = 'COM3'
 baud_rate = 9600  # Match the Arduino's baud rate
 
 
 flask_url = "http://localhost:8000"
+
+buffer = []
+N = 10
+N_local = 3
+THRESHOLD = 10
+# 0: no action, 1: next, 2: previous, 3: pause, 4: play, 5: volume up, 6: volume down
+actions = {0: "No action", 1: "Next", 2: "Previous", 3: "Pause", 4: "Play", 5: "Volume up", 6: "Volume down"}
+last_action =  {"action": actions[0], "time": datetime.now()}
+
+action_time_threshold = timedelta(seconds=1)
+
+prev_distance_time = datetime.now()
+
+distance_time_threshold = timedelta(seconds=1)
 
 # Initialize the serial connection
 try:
@@ -22,16 +41,84 @@ try:
     while True:
         if ser.in_waiting > 0:
             # Read a line from the serial port
-            line = ser.readline().decode('utf-8').strip()
+            line = float(ser.readline().decode('utf-8').strip())
             # Print the data or use it in your application
-            print(f"Distance: {line} cm")
+            # print(f"Distance: {line} cm")
 
             # Prepare data payload
-            data = {"distance": line}
+            # data = {"distance": line}
 
             # Send data to Flask backend
-            response = requests.post(f"{flask_url}/receive_data", json=data)
-            print(f"Sent to Flask, response: {response.json()}")
+            # response = requests.post(f"{flask_url}/receive_data", json=data)
+            # print(f"Sent to Flask, response: {response.json()}")
+
+            
+            current_time = datetime.now()
+            if len(buffer) < N:
+                buffer.append(line)
+            else:
+                buffer.pop(0)
+                # buffer = buffer[1:]
+                buffer.append(line)
+
+                # np.append(buffer, data)
+
+            if (current_time - prev_distance_time <= distance_time_threshold):
+                continue
+
+            buffer_np = np.array(buffer)
+            
+            prev_distance_time = current_time
+
+            smoothed = gaussian_filter1d(buffer_np, sigma=2)
+
+            local = smoothed[-N_local:]
+            local_avg = np.mean(local)
+            abs_avg = np.mean(smoothed)
+
+            diff = local_avg - abs_avg
+            deriv = np.mean(np.diff(local))
+
+            print("{last_action} diff: {diff}")
+
+            predicted_action = ""
+
+            if diff > THRESHOLD and deriv > 0:
+                predicted_action = actions[1]
+            elif diff < -THRESHOLD and deriv < 0:
+                predicted_action = actions[2]
+            elif -THRESHOLD < diff and diff < THRESHOLD:
+                predicted_action = actions[3]
+            else:
+                predicted_action = actions[0]
+
+                # # no action
+            if predicted_action == last_action["action"] and current_time - last_action["time"] <= action_time_threshold:
+                last_action["time"] = current_time
+                continue
+
+            last_action["action"] = predicted_action
+            last_action["time"] = current_time
+
+            data = {'status': f'{last_action["action"]}'}
+            print("data: ", data)
+            # response = requests.post(f"{flask_url}/receive_data", json=data)
+
+            match last_action["action"]:
+                case "Next": 
+                    print("next")
+                    response = requests.post(f"{flask_url}/skip_song")
+                case "Previous":
+                    print("previous")
+                    response = requests.post(f"{flask_url}/previous_song")
+                # case "Pause":
+                #     print("pause")
+                #     response = requests.post(f"{flask_url}/play_pause")
+                case _:
+                     continue
+
+
+
 except KeyboardInterrupt:
     print("Exiting...")
 finally:
--- spotify-stuff/spoti.py
+++ spotify-stuff/spoti.py
@@ -3,10 +3,27 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import os
 from dotenv import load_dotenv
+from scipy.ndimage import gaussian_filter1d
+import numpy as np
+
+from datetime import datetime, timedelta
 
 load_dotenv()
 
 app = Flask(__name__,  static_url_path='')
+buffer = np.array([])
+N = 10
+N_local = 3
+THRESHOLD = 10
+# 0: no action, 1: next, 2: previous, 3: pause, 4: play, 5: volume up, 6: volume down
+actions = {0: "No action", 1: "Next", 2: "Previous", 3: "Pause", 4: "Play", 5: "Volume up", 6: "Volume down"}
+last_action =  {"action": actions[0], "time": datetime.now()}
+
+action_time_threshold = timedelta(seconds=1)
+
+prev_distance_time = datetime.now()
+
+distance_time_threshold = timedelta(seconds=1)
 
 # Spotify credentials from .env
 SPOTIFY_CLIENT_ID = os.getenv('SPOTIFY_CLIENT_ID')
@@ -29,6 +46,12 @@ def home():
     return render_template('index.html')
 
 
+@app.route('/last_action', methods=['GET'])
+def get_last_action():
+    """Endpoint to return the last known action."""
+    return jsonify({"last_action": last_action["action"]})
+
+
 @app.route('/current_song', methods=['GET'])
 def current_song():
     try:
@@ -36,7 +59,9 @@ def current_song():
         if playback and playback['item']:
             artist_name = ", ".join([artist['name'] for artist in playback['item']['artists']])
             song_name = playback['item']['name']
-            return jsonify({'artist': artist_name, 'song': song_name})
+            album = playback['item']['album']['name']
+            album_img = playback['item']['album']['images'][0]['url']
+            return jsonify({'artist': artist_name, 'song': song_name, 'album': album, 'album_img': album_img})
         return jsonify({'error': 'No active playback found.'}), 404
     except Exception as e:
         return jsonify({'error': str(e)}), 500
@@ -44,44 +69,54 @@ def current_song():
 
 @app.route('/play_pause', methods=['POST'])
 def play_pause():
+    global last_action
     try:
         playback = sp.current_playback()
         if playback and playback['is_playing']:
             sp.pause_playback()
-            return jsonify({'status': 'Paused playback.'})
+            last_action["actions"] = actions[3]
+            return jsonify({'status': f'{last_action["actions"]}'})
         sp.start_playback()
-        return jsonify({'status': 'Resumed playback.'})
+        last_action["actions"] = actions[4]
+        return jsonify({'status': f'{last_action["actions"]}'})
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
 
 @app.route('/skip_song', methods=['POST'])
 def skip_song():
+    global last_action
     try:
         sp.next_track()
-        return jsonify({'status': 'Skipped to the next song.'})
+        last_action["actions"] = actions[1]
+        return jsonify({'status': f'{last_action["actions"]}'})
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
 
 @app.route('/previous_song', methods=['POST'])
 def previous_song():
+    global last_action
     try:
         sp.previous_track()
-        return jsonify({'status': 'Skipped to the previous song.'})
+        last_action["actions"] = actions[2]
+        return jsonify({'status': f'{last_action["actions"]}'})
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
 
 @app.route('/volume_up', methods=['POST'])
 def volume_up():
+    global last_action
     try:
         playback = sp.current_playback()
         if playback:
             current_volume = playback['device']['volume_percent']
             new_volume = min(current_volume + 10, 100)
+
             sp.volume(new_volume)
-            return jsonify({'status': f'Volume increased to {new_volume}%.'})
+            last_action["actions"] = actions[5]
+            return jsonify({'status': f'{last_action["actions"]}'})
         return jsonify({'error': 'No active playback found.'}), 404
     except Exception as e:
         return jsonify({'error': str(e)}), 500
@@ -89,22 +124,89 @@ def volume_up():
 
 @app.route('/volume_down', methods=['POST'])
 def volume_down():
+    global last_action
     try:
         playback = sp.current_playback()
         if playback:
             current_volume = playback['device']['volume_percent']
             new_volume = max(current_volume - 10, 0)
             sp.volume(new_volume)
-            return jsonify({'status': f'Volume decreased to {new_volume}%.'})
+            last_action["actions"] = actions[6]
+            return jsonify({'status': f'{last_action["actions"]}'})
         return jsonify({'error': 'No active playback found.'}), 404
     except Exception as e:
         return jsonify({'error': str(e)}), 500
     
 @app.route('/receive_data', methods=['POST'])
 def receive_data():
-    data = request.json  # Get the JSON data sent from the Python script
-    print(f"Received data: {data}")
-    return jsonify({"status": "success", "message": "Data received!"}), 200
+
+    print(f"Received data, {request.json['distance']}")
+    return jsonify({'status': "no action"})
+    # global last_action
+    # global buffer
+    # global prev_distance_time
+    # global distance_time_threshold
+
+    # current_time = datetime.now()
+
+    # data = request.json  # Get the JSON data sent from the Python script
+    # if len(buffer) < N:
+    #     np.append(buffer, data)
+    # else:
+    #     # buffer.pop(0)
+    #     buffer = buffer[1:]
+    #     np.append(buffer, data)
+
+    # if (current_time - prev_distance_time <= distance_time_threshold):
+    #     return jsonify({'status': "no action"})
+    
+    # prev_distance_time = current_time
+
+    # smoothed = gaussian_filter1d(buffer, sigma=2)
+
+    # local = smoothed[-N_local:]
+    # local_avg = np.mean(local)
+    # abs_avg = np.mean(smoothed)
+
+    # diff = local_avg - abs_avg
+    # deriv = np.mean(np.diff(local))
+
+    # print("{last_action} diff: {diff}")
+
+    # predicted_action = ""
+
+    # if diff > THRESHOLD and deriv > 0:
+    #     predicted_action = actions[1]
+    # elif diff < -THRESHOLD and deriv < 0:
+    #     predicted_action = actions[2]
+    # elif -THRESHOLD < diff and diff < THRESHOLD:
+    #     predicted_action = actions[3]
+    # else:
+    #     predicted_action = actions[0]
+    
+    # # no action
+    # if predicted_action == last_action["action"] and current_time - last_action["time"] <= action_time_threshold:
+    #     last_action["time"] = current_time
+    #     return jsonify({'status': f'{last_action["action"]}'})
+
+    # last_action["action"] = predicted_action
+    # last_action["time"] = current_time
+
+    # match last_action["action"]:
+    #     case "No action":
+    #         return jsonify({'status': f'{last_action["action"]}'})
+    #     case "Next":
+    #         sp.next_track()
+    #         return jsonify({'status': f'{last_action["action"]}'})
+        
+    #     case "Previous":
+    #         sp.previous_track()
+    #         return jsonify({'status': f'{last_action["action"]}'})
+    #     case "Pause":
+    #         sp.pause_playback()
+    #         return jsonify({'status': f'{last_action["action"]}'})
+    #     case _:
+    #          return jsonify({'status': f'{last_action["action"]}'})
 
 
 if __name__ == "__main__":
